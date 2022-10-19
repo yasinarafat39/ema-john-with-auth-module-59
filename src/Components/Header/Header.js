c409b965057e2ec10e2f1075ac71dc7b14befceb
yasinarafat39 <yasinarafat2557@gmail.com>
2022-10-19 13:03:13 +0600

    Log In page impleement

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <nav>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 
 const Login = () => {
     return (
-        <div>
-            <h1>this is log in page</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Login</h1>
+
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder='example@gmail.com' required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder='password' required/>
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+                <p>New to ema john?<Link to='/signup'>Create New Account</Link></p>
+            </form>
         </div>
     );
 };
